73e90f211e96a6e963770317971b2e6de246e5e5
Kalender6 <dogukankalender6@gmail.com>
2022-11-10 22:08:58 +0300

    odev5

---
+++ odev5/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:odev5/MasterDetailPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MasterDetailPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
